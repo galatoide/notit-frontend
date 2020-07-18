492f37795f0e733a145104513bc5683d0397915b
Wilson Cruz <galatoide@gmail.com>
2020-07-18 14:56:55 +0100

    AWS

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Landing
+            </div>
+         );
+    }
+}
+ 
+export default Landing;
