ee47a207c1718ff9dfebb2a390b6cbb2d7b69a8c
Wilson Cruz <galatoide@gmail.com>
2020-07-18 16:41:51 +0100

    AWS

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -27,7 +27,7 @@ export const registerUser = (userData, history) => dispatch => {
 export const loginUser = userData => dispatch => {
     dispatch(toggleUserLoading());
     axios
-       .post("/api/users/login", userData)
+       .post("https://notit-api.herokuapp.com/api/users/login", userData)
        .then(res => {
           dispatch(resetPost());
           const { token } = res.data;
